953051b02d2085b55036f42bd0b68d6082dafbe6
heaven <542248377@qq.com>
2023-08-06 16:07:34 +0800

    neovim

--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
-	description = "Heaven's NixOS settings";
-	inputs={
-		nixpkgs.url="github:NixOS/nixpkgs/nixos-unstable";
-		home-manager={
-			url="github:nix-community/home-manager/release-23.05";
-			inputs.nixpkgs.follows="nixpkgs";
-		};
-	};
+    description = "Heaven's NixOS settings";
+    inputs={
+        nixpkgs.url="github:NixOS/nixpkgs/nixos-unstable";
+        home-manager={
+            url="github:nix-community/home-manager/release-23.05";
+            inputs.nixpkgs.follows="nixpkgs";
+        };
+    };
 
-	outputs = { self, nixpkgs,home-manager,... }@inputs: {
-		nixosConfigurations={
-			"nixos"=nixpkgs.lib.nixosSystem {
-				system="x86_64-linux";
-				modules=[
-					./configuration.nix
-						home-manager.nixosModules.home-manager
-						{
-							home-manager.useGlobalPkgs=true;
-							home-manager.useUserPackages=true;
-							home-manager.users.ht=import ./home.nix;
-						}
-				];
-			};
-		};
-	};
+    outputs = { self, nixpkgs,home-manager,... }@inputs: {
+        nixosConfigurations={
+            "nixos"=nixpkgs.lib.nixosSystem {
+                system="x86_64-linux";
+                modules=[
+                    ./configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs=true;
+                            home-manager.useUserPackages=true;
+                            home-manager.users.ht=import ./home.nix;
+                        }
+                ];
+            };
+        };
+    };
 }
