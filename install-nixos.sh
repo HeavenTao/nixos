68105938ad66ada63040c9288c27ad1e23a033a4
heaven <542248377@qq.com>
2023-08-22 15:06:08 +0800

    add parted

--- install-nixos.sh
+++ install-nixos.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 disk=${1:?"disk not set"}
-bootSize=${2:-"512MB"}
-swapSize=${3:-"4096MB"}
-echo $disk $bootSize $swapSize
-parted disk -- mklabel gpt
+bootSize=${2:-"513MiB"}
+swapSize=${3:-"4096MiB"}
 
+#check disk if exists
+fdisk -l | grep $disk
+if [ "$?" = "0" ]; then
+    echo "check disk exists:${disk} exists"
+else
+    echo "${disk} not exists"
+fi
 
-
+# parted
+let endSize=${bootSize%MiB}+${swapSize%MiB}
+parted $disk mklabel gpt
+parted $disk mkpart ESP fat32 1MiB $bootSize
+parted $disk set 1 esp on
+parted $disk mkpart primary linux-swap $bootSize "${endSize}MiB"
+parted $disk mkpart primary "${endSize}Mib" 100%
+fdisk -l $disk
