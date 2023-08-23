ef97ce33d393e0e916d8244067e67cb6936b9163
heaven <542248377@qq.com>
2023-08-23 16:46:34 +0800

    add format,mount,install script

--- install-nixos.sh
+++ install-nixos.sh
@@ -19,3 +19,22 @@ parted $disk set 1 esp on
 parted $disk mkpart primary linux-swap $bootSize "${endSize}MiB"
 parted $disk mkpart primary "${endSize}MiB" 100%
 fdisk -l $disk
+
+read -p "Is this config ok?<[y]/n>?" -a args
+option=${args[0]:-"y"}
+if [ ${option} = "y"];then
+    # format
+    mkfs.fat -F 32 -n boot /dev/sda1
+    mkswap -L swap /dev/sda2
+    mkfs.ext4 -L nixos /dev/sda3
+    # mount
+    mount /dev/disk/by-label/nixos /mnt
+    mkdir -p /mnt/boot
+    mount /dev/disk/by-label/boot /mnt/boot
+    swapon /dev/sda2
+    # install
+    nixos-generate-config --root /mnt
+    nixos-install
+else
+    echo "stop"
+fi
